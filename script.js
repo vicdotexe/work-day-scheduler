193f5b1692348cbe6d26570fdd9fb0c1a7305973
victor <vicdotexe@gmail.com>
2022-10-04 20:17:59 -0700

    hacked together a function to get hourly time-relation

--- script.js
+++ script.js
@@ -6,9 +6,18 @@ function onRowClick(event){
     console.log(hour);
 }
 
+//todo: see if there's a better way to do this, feels hacked together.
+function getCurrentRelation(eventHour){
+    if (moment().isBetween(moment(eventHour), moment(eventHour).add(1,"hours"))){
+        return "present";
+    }
+    return moment().isAfter(moment(eventHour)) ? "past" : "future";
+}
+
 for(var i = 9; i < 18; i++){
 
     var eventHour = moment(i, ["H"]);
+    console.log(eventHour);
     var row = $("<div>");
     row.addClass("row");
     row.attr("data-hour", eventHour.format("h A"));
@@ -17,18 +26,14 @@ for(var i = 9; i < 18; i++){
     var timeColumn = $("<div>");
     timeColumn.addClass("col-1")
     timeColumn.addClass("hour");
+    timeColumn.text(eventHour.format("h A"));
     row.append(timeColumn);
 
-    // var relative;
-
-    // var now = moment().get("hour");
-    // var relative = moment(now).from(eventHour);
-
-    // console.log(relative);
-
+    var timeRelation = getCurrentRelation(eventHour);
     var eventsColumn = $("<div>");
     eventsColumn.addClass("col-10")
     eventsColumn.addClass("eventsColumn");
+    eventsColumn.addClass(timeRelation);
     row.append(eventsColumn);
 
     var saveColumn = $("<div>");
